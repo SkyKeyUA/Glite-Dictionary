1ac0c6aa976bfc1e5bf906e3224bba7911c434fb
OLEKSANDR VEREMIIENKO <120383111+SkyKeyUA@users.noreply.github.com>
2026-01-11 17:17:46 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,6 +16,17 @@ function documentActions(e) {
     html.classList.remove('light-mode');
     console.log('remove light-mode');
   }
+  if (window.innerWidth < 768) {
+    if (
+      targetElement.closest('.actions__btn--search') &&
+      !html.classList.contains('search-bar-open')
+    ) {
+      html.classList.add('search-bar-open');
+    }
+    if (targetElement.closest('.search-header__close')) {
+      html.classList.remove('search-bar-open');
+    }
+  }
 }
 var swiper = new Swiper('.hero-swiper', {
   spaceBetween: 20,
