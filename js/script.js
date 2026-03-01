303cbe82be8d4cbb56f49aacf16921c3d5f91874
OLEKSANDR VEREMIIENKO <120383111+SkyKeyUA@users.noreply.github.com>
2026-03-01 11:06:02 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,11 +10,9 @@ function documentActions(e) {
 
   if (targetElement.closest('.dark-light-mode__sun') && !html.classList.contains('light-mode')) {
     html.classList.add('light-mode');
-    console.log('light-mode');
   }
   if (targetElement.closest('.dark-light-mode__moon') && html.classList.contains('light-mode')) {
     html.classList.remove('light-mode');
-    console.log('remove light-mode');
   }
   if (targetElement.closest('.home-content__scroll')) {
     const video = document.querySelector('.home-content__hero-swiper--scroll-video');
@@ -23,26 +21,26 @@ function documentActions(e) {
       window.scrollTo({ top: y, behavior: 'smooth' });
     }
   }
-  if (window.innerWidth < 768) {
-    if (
-      targetElement.closest('.actions__btn--search') &&
-      !html.classList.contains('search-bar-open')
-    ) {
-      html.classList.add('search-bar-open');
-    }
-    if (targetElement.closest('.search-header__close')) {
-      html.classList.remove('search-bar-open');
-    }
+  if (
+    targetElement.closest('.actions__btn--search') &&
+    !html.classList.contains('search-bar-open')
+  ) {
+    html.classList.add('search-bar-open');
+  }
+  if (targetElement.closest('.search-header__close')) {
+    html.classList.remove('search-bar-open');
   }
 }
-var swiper = new Swiper('.hero-swiper', {
-  spaceBetween: 20,
-  pagination: {
-    el: '.swiper-actions__pagination',
-    type: 'fraction',
-  },
-  navigation: {
-    nextEl: '.swiper-actions__arrow--next',
-    prevEl: '.swiper-actions__arrow--prev',
-  },
-});
+if (document.querySelector('.hero-swiper')) {
+  var swiper = new Swiper('.hero-swiper', {
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-actions__pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-actions__arrow--next',
+      prevEl: '.swiper-actions__arrow--prev',
+    },
+  });
+}
